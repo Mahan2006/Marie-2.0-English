5fd11962fa8119bca791ccf688c9c63650a4dd76
The Style King <78583783+Style-King@users.noreply.github.com>
2021-06-23 18:38:36 +0530

    Update misc.py

--- tg_bot/modules/helper_funcs/misc.py
+++ tg_bot/modules/helper_funcs/misc.py
@@ -54,17 +54,13 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     pairs = [
         modules[i * 3:(i + 1) * 3] for i in range((len(modules) + 3 - 1) // 3)
     ]
-    if len(modules) % 2 == 1:
+
+    round_num = len(modules) / 3
+    calc = len(modules) - round(round_num)
+    if calc == 1:
         pairs.append((modules[-1],))
-
-    max_num_pages = ceil(len(pairs) / 7)
-    modulo_page = page_n % max_num_pages
-
-    # can only have a certain amount of buttons side by side
-    if len(pairs) > 7:
-        pairs = pairs[modulo_page * 7:7 * (modulo_page + 1)] + [
-            (EqInlineKeyboardButton("Channel", url="t.me/MahanMVMovies")),
-             EqInlineKeyboardButton("Group", url="t.me/MahanMVGroup")))]
+    elif calc == 2:
+        pairs.append((modules[-1],))
 
     return pairs
 
