4c15467b2199441e0a74c7d7473b3b0a03ad9a9a
Mahan M.V <73701328+Mahan2006@users.noreply.github.com>
2021-09-29 15:56:52 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -144,7 +144,7 @@ def start(bot: Bot, update: Update, args: List[str]):
         else:
             first_name = update.effective_user.first_name
             update.effective_message.reply_text(
-                PM_START_TEXT.format(escape_markdown(first_mention), escape_markdown(bot.first_name), OWNER_ID),reply_markup = InlineKeyboardMarkup(mv_buttons),
+                PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_ID),reply_markup = InlineKeyboardMarkup(mv_buttons),
                 parse_mode=ParseMode.MARKDOWN)
     else:
         update.effective_message.reply_text("waked up😏😏😏")
